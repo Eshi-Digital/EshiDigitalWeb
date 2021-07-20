42a0db991e394a71b004cc836a5e17ed4f11e8d1
RedietMengiste <se.rediet.mengiste@gmail.com>
2021-07-21 01:05:09 +0300

    feat(indexpage):Added footer for all pages

--- client/src/pages/login/index.jsx
+++ client/src/pages/login/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Navbar from '../../components/navbar/navbar';
+import FAQ from '../../components/FAQ/faq';
+import Footer from '../../components/footer/footer';
 
 const LoginPage = () => {
   return (
     <>
       <Navbar></Navbar>
       {/* <div className="app">Login</div> */}
+      {/* <FAQ></FAQ> */}
+      <Footer></Footer>
     </>
   );
 };
